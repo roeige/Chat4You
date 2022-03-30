73be8fad9c7d4984fd279b3b612848cb6f270373
roeige <roy508@madaimas.org.il>
2022-03-30 13:33:42 +0300

    started to work on left side

--- src/components/message_components/ContactList.js
+++ src/components/message_components/ContactList.js
@@ -1,17 +1,17 @@
 const ContactList = (props) => {
-  return (
-    <li className="p-2 list-group-item d-flex justify-content-between align-items-start">
-      <div> {props.picture} </div>
-      <div className="ms-2 me-auto">
-        <div className="fw-bold"> {props.name} </div>
-        {props.lastMessage}{" "}
-      </div>
-      <span className="badge bg-dark rounded-pill">
-        {" "}
-        {props.timeAgo + " minute ago"}{" "}
-      </span>
-    </li>
-  );
+    return (
+        <li className="p-2 list-group-item d-flex justify-content-between align-items-start">
+            <div> {props.picture} </div>
+            <div className="ms-2 me-auto">
+                <div className="fw-bold"> {props.name} </div>
+                {props.lastMessage}{" "}
+            </div>
+            <span className="badge bg-dark rounded-pill">
+                {" "}
+                {props.timeAgo + " minute ago"}{" "}
+            </span>
+        </li>
+    );
 };
 
 export default ContactList;
